fbb97734bce2ba2980732447417b1967e3b56db0
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-05-06 22:38:53 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,40 @@ import { GetAllUsers, GetSingleUser } from './Queries/User';
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
+import mongoose from 'mongoose';
+import { TodoModel } from './Models/Todo';
 
 const app = express();
+
+const connection = async () => { 
+    try{
+        await mongoose.connect("mongodb+srv://shehan:shehan@myfirstcluster.h9a6u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {  });
+    }
+    catch(ex){
+       return "Error : " + ex;
+    }
+};
+
+console.log("Mongo ; ", connection());
+
+
+const run = async () => {
+    try {
+        let todo = await TodoModel.create({
+            id : 1,
+            userId : 1,
+            title : "Test title",
+            completed : true
+        });
+        console.log("Todo", todo);
+    }
+    catch(ex){
+        return "Error : " + ex;
+    }
+}
+
+run();
+
 
 const rootQuery = new GraphQLObjectType({
     name : "Query",
