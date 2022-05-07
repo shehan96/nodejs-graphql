ddaaa48dc14ca545bbd6606faf90145a9c1d8591
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-05-07 18:50:57 +0530

    Update Todo.ts

--- src/Resolvers/Todo.ts
+++ src/Resolvers/Todo.ts
@@ -1,6 +1,22 @@
+import { TodoModel } from './../Models/Todo';
 import axios from "axios";
 
 export const GetTodos = async (parent, args) => {
     let result = await axios.get('https://jsonplaceholder.typicode.com/todos');
     return result.data;
 };
+
+export const InsertSingleTodo = async (parent, args) => {
+    
+    const { id, userId, title, completed } = args;
+
+    let todo = await TodoModel.create({
+        id : id,
+        userId : userId,
+        title : title,
+        completed : completed
+    });
+
+    return todo;
+
+}
